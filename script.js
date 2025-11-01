1825619e611cef273cccceb50266515714341b6f
shreyansh-sharma7 <shreyanshsharma0708@gmail.com>
2025-11-02 04:20:08 +0530

    finished

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+hamburger.classList.toggle("active");
+mobileMenu.classList.toggle("active");
+});

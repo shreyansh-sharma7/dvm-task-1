a20bb17c90088e882fc17147a515e7475e51efc4
shreyansh-sharma7 <shreyanshsharma0708@gmail.com>
2025-11-03 11:00:18 +0530

    add carousel1

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ hamburger.addEventListener("click", () => {
 hamburger.classList.toggle("active");
 mobileMenu.classList.toggle("active");
 });
+
+const carousel = document.getElementById("carousel-1-image")
+
+
+const carousel_buttons = document.querySelectorAll(".section-2 .feature-card")
+
+console.log(carousel_buttons)
+
+for (const button of carousel_buttons) {
+  button.addEventListener("click", function () {
+    carousel.src = `./assets/c-${this.id}-img.webp`;
+
+    for (const btn of carousel_buttons) {
+      btn.className = "feature-card";
+    }
+
+    this.classList.add("selected");
+  });
+}
